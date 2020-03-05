212630eddbb3bf0c66a68f749b34a4cf68b4e085
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-03-05 00:50:46 -0600

    Update templateCliente.php

--- core/helpers/templateCliente.php
+++ core/helpers/templateCliente.php
@@ -21,7 +21,6 @@ class page{
                     <img src="../../resources/img/LOGO.png" width="220" height="100" class="d-inline-block align-top" alt="Logo">
                 </a>
                 <div>
-                    <button type="button" class="btn" id="botones"><i class="fas fa-shopping-cart"></i>Carrito</button>
                     <div class="btn-group" role="group">
                     <button id="btnGroupDrop1" type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-user"></i> Cuenta
