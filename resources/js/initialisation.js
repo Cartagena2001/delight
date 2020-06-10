86183c6ad54a4211c6952701b48e6fb270e91ca0
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-06-09 18:51:05 -0600

    Categoras

--- resources/js/initialisation.js
+++ resources/js/initialisation.js
@@ -9,6 +9,28 @@ $(document).ready(function() {
  });
 
 
+
+$(document).ready(function() {
+
+  $('#archivoProducto').on('change',function(e){
+      //get the file name
+      var fileName = e.target.files[0].name;
+      //replace the "Choose a file" label
+      $(this).next('.custom-file-label').html(fileName);
+  })
+   
+});
+
+$(document).ready(function() {
+
+  $('#archivoCategoria').on('change',function(e){
+      //get the file name
+      var fileName = e.target.files[0].name;
+      //replace the "Choose a file" label
+      $(this).next('.custom-file-label').html(fileName);
+  })
+   
+});
 // graficos
 new Morris.Line({
     // ID of the element in which to draw the chart.
