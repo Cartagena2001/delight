5da0e234e822102d669fcf2a74e3d67e2eb3d8c5
SofiaRamirez <41772455+SofiaRamirez@users.noreply.github.com>
2020-07-01 23:40:36 -0600

    dd

--- core/api/cliente/noticias.php
+++ core/api/cliente/noticias.php
@@ -3,10 +3,12 @@ require_once('../../helpers/database.php');
 require_once('../../helpers/validator.php');
 require_once('../../models/noticias.php');
 
+//Designo un parametro a noticias 
 if(isset($_GET['action'])){
     $noticias = new noticias;
     $result = array('status' => 0, 'message' => null, 'exception' => null);
 
+//Case que hace contacto con el modelo
     switch ($_GET['action']){
         case 'leerNoticias':
             if($result['dataset']=$noticias->leerTodasNoticias()){
